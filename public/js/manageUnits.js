64576fde13c04d94e9f6a880f5840d27e0ac44e0
Max Wiegant <mwiegant@nevada.unr.edu>
2019-08-16 16:59:19 -0700

    solved: issue #1 - be able to create and save new units

--- public/js/manageUnits.js
+++ public/js/manageUnits.js
@@ -61,19 +61,41 @@ dndApp.controller('ManageUnitsController', ['$scope', function ($scope) {
     $scope.selectedUnit.Keywords = keywords;
   };
 
+  $scope.createUnit = function() {
+    $scope.selectedUnit = {
+        _id: -1,
+        Name : "",
+        Keywords : "",
+        ATT : 0,
+        DEF : 0,
+        PWR : 0,
+        TGH : 0,
+        MOR : 0,
+        MAX_STR : 4,
+        STR : 4,
+        MAN_ATT : 0,
+        MAN_DEF : 0,
+        MAN_PWR : 0,
+        MAN_TGH : 0,
+        MAN_MOR : 0,
+        Alive: true
+    };
+
+    $scope.sumAtt = 0;
+    $scope.sumPwr = 0;
+    $scope.sumMor = 0;
+    $scope.sumDef = 0;
+    $scope.sumTgh = 0;
+  };
+
   $scope.saveUnit = function() {
     if ($scope.selectedUnit === null)
       alert("Cannot save unit: there is no unit currently selected.");
 
     // existing units will always have an id greater than 0
     const isNewUnit = $scope.selectedUnit._id <= 0;
-    const passedValidation = true;
 
-
-    // TODO -- add validation here
-
-
-    if (passedValidation) {
+    if (isValidPortrait()) {
       if (isNewUnit)
         $scope.selectedUnit._id = Date.now();
 
@@ -86,11 +108,16 @@ dndApp.controller('ManageUnitsController', ['$scope', function ($scope) {
         else {
           $scope.saveMessage = "Successfully saved this unit!";
 
-          // update the saved unit in the availableUnits list, so the unit's card will update
-          for (let i = 0; i < $scope.availableUnits.length; i++) {
-            if ($scope.availableUnits[i]._id === $scope.selectedUnit._id) {
-              $scope.availableUnits[i] = Object.assign({}, $scope.selectedUnit);
-              break;
+          if (isNewUnit) {
+            $scope.availableUnits.push(Object.assign({}, $scope.selectedUnit));
+          }
+          else {
+            // update the saved unit in the availableUnits list, so the unit's card will update
+            for (let i = 0; i < $scope.availableUnits.length; i++) {
+              if ($scope.availableUnits[i]._id === $scope.selectedUnit._id) {
+                $scope.availableUnits[i] = Object.assign({}, $scope.selectedUnit);
+                break;
+              }
             }
           }
         }
@@ -99,6 +126,25 @@ dndApp.controller('ManageUnitsController', ['$scope', function ($scope) {
       });
     }
   };
+
+  function isValidPortrait() {
+    let isValid = false;
+
+    if ($scope.selectedUnit.Name === undefined || $scope.selectedUnit.Name.length == 0) {
+      $scope.saveSuccessful = false;
+      $scope.saveMessage = "Error saving unit: the unit must have a name.";
+    }
+
+    // TODO -- add validation for keywords once that functionality is working
+    // else if () {}
+
+    else {
+      isValid = true;
+    }
+
+
+    return isValid;
+  }
 
   function loadSelectedKeywords(_keywords) {
     const ANCESTRY = 0;
