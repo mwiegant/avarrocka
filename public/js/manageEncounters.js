c4c443bb0dee5e2271760d14f66f24577d2d400d
Max Wiegant <mwiegant@nevada.unr.edu>
2019-09-29 17:53:54 -0700

    now showing all fields for the selected encounter

--- public/js/manageEncounters.js
+++ public/js/manageEncounters.js
@@ -1,32 +1,61 @@
 dndApp.controller('ManageEncountersController', ['$scope', function ($scope) {
 
   $scope.availableEncounters = [];
+  $scope.availableUnits = [];
   $scope.selectedEncounter = null;
+  $scope.selectedHostileUnit = null;
   $scope.saveSuccessful = null;
   $scope.saveMessage = null;
 
-  var init = function() {
+  $scope.selectEncounter = function(encounter) {
+
+    // deep copy, so that un-saved changes will completely disappear when a new unit is selected
+    $scope.selectedEncounter = Object.assign({}, encounter);
+
+    // shallow copy, so changing the selected hostile unit will immediately persist onto the selected encounter
+    $scope.selectedHostileUnit = $scope.selectedEncounter.SelectedHostile;
+  };
+
+  $scope.selectHostileUnit = function(hostileUnit) {
+    $scope.selectedHostileUnit = hostileUnit;
+  };
+
+  $scope.createEncounter = function() {
+    $scope.selectedEncounter = {
+      _id: -1,
+      Name : "",
+      Status : "Disabled",
+      SelectedHostile : null,
+      Rewards : "",
+      Deleted : false
+    };
+
+    $scope.selectedHostileUnit = null;
+  };
+
+  function init() {
 
     encountersClient.loadEncounters(null, function(encounters) {
       $scope.availableEncounters = encounters;
 
-      // per encounter, per SelectedHostile, get the name of the hostile unit
+      // get all the hostile units, so that they are available when editing encounters
+      unitsClient.loadHostiles(null, function(units) {
+        $scope.availableUnits = units;
+        $scope.$apply();
+      });
+
+      // per encounter, hostile unit id ---> full hostile unit data
       $scope.availableEncounters.forEach(function(encounter) {
 
-        for (let i = 0; i < encounter.SelectedHostiles.length; i++) {
-          unitsClient.getHostileUnit(encounter.SelectedHostiles[i], false, function(hostile) {
-            encounter.SelectedHostiles[i] = hostile.Name;
-            $scope.$apply();
-          });
-        }
-
-
-
+        unitsClient.getHostileUnit(encounter.SelectedHostile, false, function (hostile) {
+          encounter.SelectedHostile = hostile;
+          $scope.$apply();
+        });
       });
 
       $scope.$apply();
     });
-  };
+  }
 
   init();
 }]);
