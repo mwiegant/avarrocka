fa98cc5c2574a82df6b9f45a9b8aae6a861bc751
Max Wiegant <mwiegant@nevada.unr.edu>
2019-10-04 16:04:21 -0700

    unsaved keyword changes no longer affect unit cards; other keyword cleanup

--- public/js/common/unitConstants.js
+++ public/js/common/unitConstants.js
@@ -4,60 +4,61 @@
 
 var unitConstants = {};
 
-unitConstants.ancestry = [
-  // key: Ancestry, values: [Attack, Power, Defense, Toughness, Morale, [Traits]]
-  { key: "Bugbear", values: [2, 0, 0, 0, 1, ["Martial"]] },
-  { key: "Dragonborn", values: [2, 2, 1, 1, 1, ["Courageous"]] },
-  { key: "Dwarf", values: [3, 1, 1, 1, 2, ["Stalwart"]] },
-  { key: "Elf", values: [2, 0, 0, 0, 1, ["Eternal"]] },
-  { key: "Elf Winged", values: [1, 1, 0, 0, 1, ["Eternal"]] },
-  { key: "Ghoul", values: [-1, 0, 2, 2, 0, ["Undead", "Horrify", "Ravenous"]] },
-  { key: "Gnoll", values: [2, 0, 0, 0, 1, ["Frenzy"]] },
-  { key: "Gnome", values: [1, -1, 1, -1, 1, []] },
-  { key: "Goblin", values: [-1, -1, 1, -1, 0, []] },
-  { key: "Hobgoblin", values: [2, 0, 0, 0, 1, ["Bred_For_War", "Martial"]] },
-  { key: "Human", values: [2, 0, 0, 0, 1, ["Courageous"]] },
-  { key: "Kobold", values: [-1, -1, 1, -1, -1, []] },
-  { key: "Lizardfolk", values: [2, 1, -1, 1, 1, ["Amphibious"]] },
-  { key: "Ogre", values: [0, 2, 0, 2, 1, ["Brutal"]] },
-  { key: "Orc", values: [2, 1, 1, 1, 2, ["Savage"]] },
-  { key: "Skeleton", values: [-2, -1, 1, 1, 1, ["Undead", "Mindless"]] },
-  { key: "Treant", values: [0, 2, 0, 2, 0, ["Regenerate"]] },
-  { key: "Zombie", values: [-2, 0, 2, 2, 2, ["Undead", "Mindless"]] }
-];
+unitConstants.ancestry = {
+  // Ancestry: [Attack, Power, Defense, Toughness, Morale, [Traits]]
+  Bugbear: [2, 0, 0, 0, 1, ["Martial"]],
+  Dragonborn: [2, 2, 1, 1, 1, ["Courageous"]],
+  Dwarf: [3, 1, 1, 1, 2, ["Stalwart"]],
+  Elf: [2, 0, 0, 0, 1, ["Eternal"]],
+  Elf_Winged: [1, 1, 0, 0, 1, ["Eternal"]],
+  Ghoul: [-1, 0, 2, 2, 0, ["Undead", "Horrify", "Ravenous"]],
+  Gnoll: [2, 0, 0, 0, 1, ["Frenzy"]],
+  Gnome: [1, -1, 1, -1, 1, []],
+  Goblin: [-1, -1, 1, -1, 0, []],
+  Hobgoblin: [2, 0, 0, 0, 1, ["Bred_For_War", "Martial"]],
+  Human: [2, 0, 0, 0, 1, ["Courageous"]],
+  Kobold: [-1, -1, 1, -1, -1, []],
+  Lizardfolk: [2, 1, -1, 1, 1, ["Amphibious"]],
+  Ogre: [0, 2, 0, 2, 1, ["Brutal"]],
+  Orc: [2, 1, 1, 1, 2, ["Savage"]],
+  Skeleton: [-2, -1, 1, 1, 1, ["Undead", "Mindless"]],
+  Treant: [0, 2, 0, 2, 0, ["Regenerate"]],
+  Zombie: [-2, 0, 2, 2, 2, ["Undead", "Mindless"]]
+};
 
 unitConstants.traits = [
   // lol. yeah I'll do this some other time.
 ];
 
-unitConstants.experience = [
-  // key: Experience, values: [Attack, Power, Defense, Toughness, Morale]
-  { key: "Green", values: [0, 0, 0, 0, 0] },
-  { key: "Regular", values: [1, 0, 0, 1, 1] },
-  { key: "Seasoned", values: [1, 0, 0, 1, 2] },
-  { key: "Veteran", values: [1, 0, 0, 1, 3] },
-  { key: "Elite", values: [2, 0, 0, 2, 4] },
-  { key: "Super Elite", values: [2, 0, 0, 2, 5] }
-];
+unitConstants.experience = {
+  // Experience: [Attack, Power, Defense, Toughness, Morale]
+  Green: [0, 0, 0, 0, 0],
+  Regular: [1, 0, 0, 1, 1],
+  Seasoned: [1, 0, 0, 1, 2],
+  Veteran: [1, 0, 0, 1, 3],
+  Elite: [2, 0, 0, 2, 4],
+  Super_Elite: [2, 0, 0, 2, 5]
+};
 
-unitConstants.equipment = [
-  // key: Equipment, values: [Attack, Power, Defense, Toughness, Morale]
-  { key: "Light", values: [0, 1, 1, 0, 0] },
-  { key: "Medium", values: [0, 2, 2, 0, 0] },
-  { key: "Heavy", values: [0, 4, 4, 0, 0] },
-  { key: "Super Heavy", values: [0, 6, 6, 0, 0] }
-];
+unitConstants.equipment = {
+  // Equipment: [Attack, Power, Defense, Toughness, Morale]
+  Light: [0, 1, 1, 0, 0],
+  Medium: [0, 2, 2, 0, 0],
+  Heavy: [0, 4, 4, 0, 0],
+  Super_Heavy: [0, 6, 6, 0, 0]
+};
 
-unitConstants.unitType = [
-  // key: Type, values: [Attack, Power, Defense, Toughness, Morale, CostModifier]
-  { key: "Flying", values: [0, 0, 0, 0, 3, 2.00] },
-  { key: "Archers", values: [0, 1, 0, 0, 1, 1.75] },
-  { key: "Cavalry", values: [1, 1, 0, 0, 2, 1.50] },
-  { key: "Levies", values: [0, 0, 0, 0, -1, 0.75] },
-  { key: "Infantry", values: [0, 0, 1, 1, 0, 1.00] },
-  { key: "Siege Engine", values: [1, 1, 0, 1, 0, 1.50] }
-];
+unitConstants.unitType = {
+  // Type: [Attack, Power, Defense, Toughness, Morale, CostModifier]
+  Flying: [0, 0, 0, 0, 3, 2.00],
+  Archers: [0, 1, 0, 0, 1, 1.75],
+  Cavalry: [1, 1, 0, 0, 2, 1.50],
+  Levies: [0, 0, 0, 0, -1, 0.75],
+  Infantry: [0, 0, 1, 1, 0, 1.00],
+  Siege_Engine: [1, 1, 0, 1, 0, 1.50]
+};
 
+// todo -- update these constants to have the same format as the objects above
 unitConstants.size = [
   // This is the only set of constants that do not follow the book verbatim,
   // although I do honor the written Size-to-CostModifier values
--- public/js/manageUnits.js
+++ public/js/manageUnits.js
@@ -1,20 +1,17 @@
 dndApp.controller('ManageUnitsController', ['$scope', function ($scope) {
 
+  const UNKNOWN = "?";
+
   $scope.availableUnits = [];
   $scope.selectedUnit = null;
   $scope.saveSuccessful = null;
   $scope.saveMessage = null;
 
-  $scope.ancestryOptions = null;
-  $scope.experienceOptions = null;
-  $scope.equipmentOptions = null;
-  $scope.unitTypeOptions = null;
+  $scope.ancestryOptions = [];
+  $scope.experienceOptions = [];
+  $scope.equipmentOptions = [];
+  $scope.unitTypeOptions = [];
   $scope.size = null; // unsure if I need this...will keep for now
-
-  $scope.selectedAncestry = null;
-  $scope.selectedExperience = null;
-  $scope.selectedEquipment = null;
-  $scope.selectedUnitType = null;
 
   $scope.sumAtt = 0;
   $scope.sumPwr = 0;
@@ -29,7 +26,9 @@ dndApp.controller('ManageUnitsController', ['$scope', function ($scope) {
     // deep copy, so that un-saved changes will completely disappear when a new unit is selected
     $scope.selectedUnit = Object.assign({}, unit);
 
-    loadSelectedKeywords(unit.Keywords);
+    // display keywords
+    $scope.selectedUnit.Keywords.Unsaved = $scope.selectedUnit.Keywords.Saved;
+    loadSelectedKeywords($scope.selectedUnit.Keywords);
 
     $scope.sumAtt = 0;
     $scope.sumPwr = 0;
@@ -51,23 +50,29 @@ dndApp.controller('ManageUnitsController', ['$scope', function ($scope) {
   };
 
   $scope.refreshKeywords = function() {
-    // TODO - fix this code (for whatever reason, the selected options are not being updated in the model)
+    let keywords = $scope.selectedUnit.Keywords;
+    let keywordsComplete = "";
 
-    let keywords = "";
+    keywordsComplete += (keywords.Ancestry || UNKNOWN);
+    keywordsComplete += " " + (keywords.Experience || UNKNOWN);
+    keywordsComplete += " " + (keywords.Equipment || UNKNOWN);
+    keywordsComplete += " " + (keywords.Type || UNKNOWN);
 
-    keywords += $scope.selectedAncestry.key;
-    keywords += " " + $scope.selectedExperience.key;
-    keywords += " " + $scope.selectedEquipment.key;
-    keywords += " " + $scope.selectedUnitType.key;
-
-    $scope.selectedUnit.Keywords = keywords;
+    $scope.selectedUnit.Keywords.Unsaved = keywordsComplete;
   };
 
   $scope.createUnit = function() {
     $scope.selectedUnit = {
         _id: -1,
         Name : "",
-        Keywords : "",
+        Keywords : {
+          Saved: "",
+          Unsaved: "",
+          Ancestry: null,
+          Experience: null,
+          Equipment: null,
+          Type: null
+        },
         ATT : 0,
         DEF : 0,
         PWR : 0,
@@ -90,19 +95,27 @@ dndApp.controller('ManageUnitsController', ['$scope', function ($scope) {
     $scope.sumTgh = 0;
   };
 
+  // TODO -- when saving unit, set selectedUnit.Keywords = selectedUnit.Keywords.Complete
   $scope.saveUnit = function() {
     if ($scope.selectedUnit === null)
       alert("Cannot save unit: there is no unit currently selected.");
 
     // existing units will always have an id greater than 0
     const isNewUnit = $scope.selectedUnit._id <= 0;
+    let keywordsObject = $scope.selectedUnit.Keywords;
 
     if (isValidPortrait()) {
       if (isNewUnit)
         $scope.selectedUnit._id = Date.now();
 
+      // prior to saving, must set the keywords to be a string, not an object
+      $scope.selectedUnit.Keywords = keywordsObject.Unsaved;
+
       unitsClient.saveUnit($scope.selectedUnit, isNewUnit, false, function(response) {
+        $scope.selectedUnit.Keywords = keywordsObject;
         $scope.saveSuccessful = response.success;
+
+        $scope.selectedUnit.Keywords.Saved = $scope.selectedUnit.Keywords.Unsaved;
 
         if (response.success === false) {
           $scope.saveMessage = "Failed to save this unit!";
@@ -148,50 +161,78 @@ dndApp.controller('ManageUnitsController', ['$scope', function ($scope) {
     return isValid;
   }
 
-  function loadSelectedKeywords(_keywords) {
+  function loadSelectedKeywords(keywords) {
     const ANCESTRY = 0;
     const EXPERIENCE = 1;
     const EQUIPMENT = 2;
     const UNIT_TYPE = 3;
 
     // keywords should always have this order: Ancestry Experience Equipment UnitType
-    const keywords = _keywords.split(" ");
+    let splitKeywords = keywords.Unsaved.split(' ');
 
-    for (let i = 0; i < unitConstants.ancestry.length; i++) {
-      if (unitConstants.ancestry[i].key == keywords[ANCESTRY]) {
-        $scope.selectedAncestry = unitConstants.ancestry[i];
-      }
-    }
+    if ($scope.ancestryOptions.includes(splitKeywords[ANCESTRY]))
+      keywords.Ancestry = splitKeywords[ANCESTRY];
+    else
+      keywords.Ancestry = UNKNOWN;
 
-    for (let i = 0; i < unitConstants.experience.length; i++) {
-      if (unitConstants.experience[i].key == keywords[EXPERIENCE]) {
-        $scope.selectedExperience = unitConstants.experience[i];
-      }
-    }
+    if ($scope.experienceOptions.includes(splitKeywords[EXPERIENCE]))
+      keywords.Experience = splitKeywords[EXPERIENCE];
+    else
+      keywords.Experience = UNKNOWN;
 
-    for (let i = 0; i < unitConstants.equipment.length; i++) {
-      if (unitConstants.equipment[i].key == keywords[EQUIPMENT]) {
-        $scope.selectedEquipment = unitConstants.equipment[i];
-      }
-    }
+    if ($scope.equipmentOptions.includes(splitKeywords[EQUIPMENT]))
+      keywords.Equipment = splitKeywords[EQUIPMENT];
+    else
+      keywords.Equipment = UNKNOWN;
 
-    for (let i = 0; i < unitConstants.unitType.length; i++) {
-      if (unitConstants.unitType[i].key == keywords[UNIT_TYPE]) {
-        $scope.selectedUnitType = unitConstants.unitType[i];
-      }
-    }
+    if ($scope.unitTypeOptions.includes(splitKeywords[UNIT_TYPE]))
+      keywords.Type = splitKeywords[UNIT_TYPE];
+    else
+      keywords.Type = UNKNOWN;
+
+    // for (let i = 0; i < unitConstants.ancestry.length; i++) {
+    //   if (unitConstants.ancestry[i].key == splitKeywords[ANCESTRY]) {
+    //     keywords.Ancestry = unitConstants.ancestry[i].key;
+    //   }
+    // }
+
+    // for (let i = 0; i < unitConstants.experience.length; i++) {
+    //   if (unitConstants.experience[i].key == splitKeywords[EXPERIENCE]) {
+    //     keywords.Experience = unitConstants.experience[i].key;
+    //   }
+    // }
+    //
+    // for (let i = 0; i < unitConstants.equipment.length; i++) {
+    //   if (unitConstants.equipment[i].key == splitKeywords[EQUIPMENT]) {
+    //     keywords.Equipment = unitConstants.equipment[i].key;
+    //   }
+    // }
+    //
+    // for (let i = 0; i < unitConstants.unitType.length; i++) {
+    //   if (unitConstants.unitType[i].key == splitKeywords[UNIT_TYPE]) {
+    //     keywords.Type = unitConstants.unitType[i].key;
+    //   }
+    // }
   }
 
   function init() {
-
     // load constants into the scope, so drop-downs will be populated
-    $scope.ancestryOptions = unitConstants.ancestry;
-    $scope.experienceOptions = unitConstants.experience;
-    $scope.equipmentOptions = unitConstants.equipment;
-    $scope.unitTypeOptions = unitConstants.unitType;
+    $scope.ancestryOptions = Object.keys(unitConstants.ancestry);
+    $scope.experienceOptions = Object.keys(unitConstants.experience);
+    $scope.equipmentOptions = Object.keys(unitConstants.equipment);
+    $scope.unitTypeOptions = Object.keys(unitConstants.unitType);
 
     unitsClient.loadUnits(null, function(units) {
-      $scope.availableUnits = units;
+
+      units.forEach(function(unit) {
+        // prior to adding each unit to the page, must set the keywords to be an object, not a string
+        unit.Keywords = {
+          Saved: unit.Keywords
+        };
+
+        $scope.availableUnits.push(unit);
+      });
+
       $scope.$apply();
     });
   }
