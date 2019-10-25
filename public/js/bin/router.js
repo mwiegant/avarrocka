0bf082ed5afad0e32364c9f3eb9a6ae7223af5c2
Max Wiegant <mwiegant@nevada.unr.edu>
2019-10-25 16:37:03 -0700

    go to the manage-units page by default

--- public/js/bin/router.js
+++ public/js/bin/router.js
@@ -13,7 +13,11 @@ dndApp.config(['$routeProvider',
           templateUrl: 'partials/manage-encounters.html',
           controller: 'ManageEncountersController'
         })
-        .when('/', {})
+        .when('/', {
+          // Go to the manage-units page by default
+          templateUrl: 'partials/manage-units.html',
+          controller: 'ManageUnitsController'
+        })
         .otherwise({
           redirectTo: 'partials/error.html',
           templateUrl: 'partials/error.html'
